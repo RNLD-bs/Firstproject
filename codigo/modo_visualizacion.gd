9a975eb4d2ed23c5e2fc3bee5d9be2a2fca54e13
RNLD-bs <amgmeneses2010@gmail.com>
2024-12-27 15:08:04 -0500

    test

--- codigo/modo_visualizacion.gd
+++ codigo/modo_visualizacion.gd
@@ -2,4 +2,7 @@ extends Node
 
 func _input(event) :
 	if event.is_action_pressed("cambiar_visualizacion"):
-		print("xd")
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_EXCLUSIVE_FULLSCREEN)
+		else :
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
